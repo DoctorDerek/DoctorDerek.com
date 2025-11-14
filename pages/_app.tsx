c7429c8289bacb01e4420905cf07171dad289531
Dr. Derek Austin <derekraustin@gmail.com>
2025-11-14 15:02:39 -0600

    refactor(app): delete all unused files and simplify back to a standard `pages/_app.tsx` file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,7 @@
 import "@/styles/globals.css"
 
-import { QueryClient, QueryClientProvider } from "react-query"
-
-import GlobalStateContext from "@/components/GlobalStateContext"
-import authMachine from "@/utils/authMachine"
-import { useInterpret } from "@xstate/react"
-
 import type { AppProps } from "next/app"
 
-const queryClient = new QueryClient()
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  const authService = useInterpret(authMachine)
-
-  // Note that we've loaded up react-query here but aren't actually using it.
-  return (
-    <QueryClientProvider client={queryClient} contextSharing={true}>
-      <GlobalStateContext.Provider value={{ authService }}>
-        {/* We can access React context throughout the app via Provider */}
-        <Component {...pageProps} />
-      </GlobalStateContext.Provider>
-    </QueryClientProvider>
-  )
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }

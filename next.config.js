6a3c66b85cb2ce892ea7b30c84254300a6fe6645
Dr. Derek Austin <derekraustin@gmail.com>
2025-12-09 12:05:36 -0600

    chore(next): remove unused `images` from `next.config.js`

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    // Configure next/image to use the website returned by the GraphQL API:
-    domains: ["img.pokemondb.net"],
-  },
 }
 
 module.exports = nextConfig

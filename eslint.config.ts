0bdb12c45130d24d0a3a5009068d0d4a01d04694
Dr. Derek Austin <derekraustin@gmail.com>
2025-12-10 16:29:58 -0600

    chore(deps): WIP update ESLint script -- run the Next.js v16 codemod using `--force` to fix the ESLint config -- WIP since Prettier / ESLint compatibility is almost certainly broken

---
+++ eslint.config.ts
@@ -0,0 +1,27 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+
+import { FlatCompat } from "@eslint/eslintrc"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, ...compat.extends("prettier"), {
+  rules: {
+    /**
+     * I have to turn this to "warn" for compatibility with Cursor since
+     * ESLint has a parsing bug around importing and use of Error types.
+     * */
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}]
+
+export default eslintConfig

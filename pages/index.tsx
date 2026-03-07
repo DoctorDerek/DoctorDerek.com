a35959f8d2dd770002f388939489988fdf953063
Dr. Derek Austin <derekraustin@gmail.com>
2026-03-06 23:16:42 -0600

    fix(ui): remove opaque background colors from fullpage sections to reveal GlobalBackground

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,36 +41,36 @@ function DisplaySections({
       render={() => {
         return (
           <ReactFullpage.Wrapper>
-            <div className="section h-screen bg-[#FFE366]">
+            <div className="section h-screen">
               <TopSection />
             </div>
 
-            <div className="section intro flex h-screen flex-col bg-[#FFE366] md:flex-row">
+            <div className="section intro flex h-screen flex-col md:flex-row">
               <IntroSection />
             </div>
 
-            <div className="section flex h-screen flex-col bg-[#b9e3ff] md:flex-row">
+            <div className="section flex h-screen flex-col md:flex-row">
               <AboutSection />
             </div>
 
-            <div className="section flex h-screen flex-col bg-[#b9e3ff]">
+            <div className="section flex h-screen flex-col">
               <AiConsultancySection />
             </div>
 
-            <div className="section bg-[#FFE366]">
+            <div className="section">
               <WorkExperienceSection />
             </div>
 
             {/*========= TESIMONIALS SECTION ========= */}
-            <div className="section bg-[#FB70AA]">
+            <div className="section">
               <Testimonials />
             </div>
 
-            <div className="section bg-[#F38B57]">
+            <div className="section">
               <BlogSection posts={posts} />
             </div>
 
-            <div className="section bg-[#F38B57]">
+            <div className="section">
               <PostsSection posts={posts} />
             </div>
 

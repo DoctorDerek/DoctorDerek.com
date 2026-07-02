3cb1ecd73248f415bf4f07a5701892e825820495
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-01 21:51:12 -0600

    chore(lint): integrate eslint-plugin-only-warn to streamline local development

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,17 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals"
 import nextTypescript from "eslint-config-next/typescript"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
+import onlyWarn from "eslint-plugin-only-warn"
 
 const eslintConfig = [
   ...nextCoreWebVitals,
   ...nextTypescript,
   eslintConfigPrettier,
+  {
+    plugins: {
+      "only-warn": onlyWarn,
+    },
+  },
   {
     ignores: [
       "node_modules/**",

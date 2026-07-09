29f02245ea48f064c3ab4e96017a5d5336b041c9
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 18:42:40 -0600

    fix(lint): temporarily disable next/typescript lint rules due to TS7 bug

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,21 +1,20 @@
+/**
+ * ONE-TIME EXCEPTION TO NO CODE COMMENT RULE:
+ * typescript-eslint (v8.63.0) is broken with TypeScript 7 (v7.0.2)
+ * TODO Restore typescript-eslint rules as soon as possible!!
+ * */
 import gitignore from "eslint-config-flat-gitignore"
-import nextConfig from "eslint-config-next"
+// import nextConfig from "eslint-config-next"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
 import onlyWarn from "eslint-plugin-only-warn"
 
 const eslintConfig = [
   gitignore(),
-  ...nextConfig,
+  // ...nextConfig,
   eslintConfigPrettier,
   {
     plugins: {
       "only-warn": onlyWarn,
-    },
-  },
-  {
-    files: ["components/ClientShell.tsx"],
-    rules: {
-      "@typescript-eslint/no-require-imports": "off",
     },
   },
 ]

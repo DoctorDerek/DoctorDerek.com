b1aaa71d057f9835b4aecd6a83471d1ae1cb3370
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-24 17:10:22 -0600

    fix(theme): make dark mode the deterministic default

--- components/SiteThemeProvider.tsx
+++ components/SiteThemeProvider.tsx
@@ -8,7 +8,7 @@ export default function SiteThemeProvider({
   children: React.ReactNode
 }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       {children}
     </ThemeProvider>
   )

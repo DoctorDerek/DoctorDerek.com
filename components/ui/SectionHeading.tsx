b316c38e11ac498424d0880444b382062e86c8ee
Dr. Derek Austin <derekraustin@gmail.com>
2026-08-07 14:25:23 -0600

    refactor(headings): remove decorative tilt interaction

--- components/ui/SectionHeading.tsx
+++ components/ui/SectionHeading.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import { ReactNode } from "react"
-import Tilt from "react-parallax-tilt"
-import { useMotionPreference } from "@/components/MotionPreferenceProvider"
 import classNames from "@/utils/classNames"
 
 type SectionHeadingProps = {
@@ -14,21 +12,9 @@ export default function SectionHeading({
   children,
   className,
 }: SectionHeadingProps) {
-  const { shouldReduceMotion } = useMotionPreference()
-
   return (
     <div className={classNames("section-heading-entrance w-max", className)}>
-      <Tilt
-        className="w-max cursor-pointer"
-        tiltEnable={!shouldReduceMotion}
-        tiltMaxAngleX={10}
-        tiltMaxAngleY={10}
-        perspective={1000}
-        scale={1.02}
-        transitionSpeed={1000}
-      >
-        {children}
-      </Tilt>
+      {children}
     </div>
   )
 }
